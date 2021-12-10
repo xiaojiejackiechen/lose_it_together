002926a60656b2358165d466eef3f75825629675
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:37 +0000

    Whitelist columns in MealComment for ActiveAdmin

--- app/admin/meal_comments.rb
+++ app/admin/meal_comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MealComment do
 
+ permit_params :meal_id, :commenter_user_id, :comment_text
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
