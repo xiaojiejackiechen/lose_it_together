a6e4f7986a9b0c5309116ba21da36be9d5b4ae36
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:43 +0000

    Add foreign key weight_tracker_user_id reference to workouts table

---
+++ db/migrate/20211210155643_add_weight_tracker_user_reference_to_workouts.rb
@@ -0,0 +1,7 @@
+class AddWeightTrackerUserReferenceToWorkouts < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :workouts, :weight_tracker_profiles, column: :weight_tracker_user_id
+    add_index :workouts, :weight_tracker_user_id
+    change_column_null :workouts, :weight_tracker_user_id, false
+  end
+end
