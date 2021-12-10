dd66faa0b9d767413f1567741f9677ffa0f81838
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:24 +0000

    Whitelist columns in WeightTrackerProfile for ActiveAdmin

--- app/admin/weight_tracker_profiles.rb
+++ app/admin/weight_tracker_profiles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WeightTrackerProfile do
 
+ permit_params :username, :target_daily_calories, :target_weight_in_lb, :profile_pic, :associated_personal_trainer_id, :associated_nutritionist_id, :login_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
