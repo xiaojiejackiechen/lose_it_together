78180accb5101917c06519ac688fa50d06491e9c
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:45 +0000

    Add foreign key weight_tracker_user_id reference to weight_daily_entries table

---
+++ db/migrate/20211210155645_add_weight_tracker_user_reference_to_weight_daily_entries.rb
@@ -0,0 +1,7 @@
+class AddWeightTrackerUserReferenceToWeightDailyEntries < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :weight_daily_entries, :weight_tracker_profiles, column: :weight_tracker_user_id
+    add_index :weight_daily_entries, :weight_tracker_user_id
+    change_column_null :weight_daily_entries, :weight_tracker_user_id, false
+  end
+end
