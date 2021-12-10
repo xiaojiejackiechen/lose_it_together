31eade19decbc4556f80ffdeafb8f0dbc1ef03ce
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:35 +0000

    Whitelist columns in WorkoutComment for ActiveAdmin

--- app/admin/workout_comments.rb
+++ app/admin/workout_comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WorkoutComment do
 
+ permit_params :workout_id, :commenter_user_id, :comment_text
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
