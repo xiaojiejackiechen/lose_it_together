2f55925d2324454730e1616fdc11fee41f0bc210
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:22 +0000

    Whitelist columns in WeightDailyEntry for ActiveAdmin

--- app/admin/weight_daily_entries.rb
+++ app/admin/weight_daily_entries.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WeightDailyEntry do
 
+ permit_params :weight_tracker_user_id, :weight_in_lb, :recorded_date
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
