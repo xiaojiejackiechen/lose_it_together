936a79e1aa1aaeb0602ecf80f68c94dfcfdd41c1
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:49 +0000

    Add foreign key associated_nutritionist_id reference to weight_tracker_profiles table

---
+++ db/migrate/20211210155649_add_nutritionist_reference_to_weight_tracker_profiles.rb
@@ -0,0 +1,7 @@
+class AddNutritionistReferenceToWeightTrackerProfiles < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :weight_tracker_profiles, :nutritionist_profiles, column: :associated_nutritionist_id
+    add_index :weight_tracker_profiles, :associated_nutritionist_id
+    change_column_null :weight_tracker_profiles, :associated_nutritionist_id, false
+  end
+end
