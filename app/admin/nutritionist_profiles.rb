e44b79d98cb4d92128f255d0198eaee60cc51d38
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:33 +0000

    Whitelist columns in NutritionistProfile for ActiveAdmin

--- app/admin/nutritionist_profiles.rb
+++ app/admin/nutritionist_profiles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register NutritionistProfile do
 
+ permit_params :rating, :login_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
