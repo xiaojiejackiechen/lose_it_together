b7bf5df5e5bd9ab87119a6e011fe6f62c0a6ad90
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:20 +0000

    Whitelist columns in Workout for ActiveAdmin

--- app/admin/workouts.rb
+++ app/admin/workouts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Workout do
 
+ permit_params :workout_date, :weight_tracker_user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
