e53855489c7de187ad3a97c9db273fbf2d6747ee
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 15:56:26 +0000

    Whitelist columns in Exercise for ActiveAdmin

--- app/admin/exercises.rb
+++ app/admin/exercises.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Exercise do
 
+ permit_params :workout_id, :excercise_id, :number_of_rep, :calorie_burned
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
